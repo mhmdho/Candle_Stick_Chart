036fb16046cc95c08521555e4964f0df74c07ad6
mhmdho <hadiomidm@gmail.com>
2022-05-17 16:27:55 +0430

    Add managers.py to user app / Edit CustomUser manager

--- apps/user/models.py
+++ apps/user/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import RegexValidator
+from .managers import CustomUserManager
 
 
 # Create your models here.
@@ -12,7 +13,7 @@ class CustomUser(AbstractUser):
     Customize django user and add phone & is_phone_verified to it.
     """
     username = ''
-    
+
     email = models.EmailField(_('email address'), unique=True)
     
     phone_regex = RegexValidator(regex=r'^09\d{9}$', message="Phone number must be entered in the format: '09121234567'.")
@@ -28,6 +29,8 @@ class CustomUser(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['phone']
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
