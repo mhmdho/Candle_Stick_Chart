464859e465ff07c127188f24c71d12f36a9445e5
mhmdho <hadiomidm@gmail.com>
2022-05-17 13:47:57 +0430

    models.py: Create User model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from django.core.validators import RegexValidator
+
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    """
+    Customize django user and add phone & is_phone_verified to it.
+    """
+    email = models.EmailField(_('email address'), unique=True)
+    
+    phone_regex = RegexValidator(regex=r'^09\d{9}$', message="Phone number must be entered in the format: '09121234567'.")
+    phone = models.CharField(validators=[phone_regex], max_length=11, unique=True) # validators should be a list
+    
+    last_login = models.DateTimeField(auto_now=True)
+
+    is_phone_verified = models.BooleanField(
+        _('phone status'),
+        default=False,
+        help_text=_('Designates whether the phone verified.'),
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone']
+
+    def __str__(self):
+        return self.email
