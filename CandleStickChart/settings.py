660bdd600013eba65dc650299b9a8f958f88e31d
mhmdho <hadiomidm@gmail.com>
2022-05-17 19:31:55 +0430

    settings.py: set bearer setting for swagger

--- CandleStickChart/settings.py
+++ CandleStickChart/settings.py
@@ -135,3 +135,13 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.CustomUser'
+
+SWAGGER_SETTINGS = {
+   'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    }
+}
