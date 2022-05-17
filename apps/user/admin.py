f0dc3fbbcb6b7686952d063f764f78a564f30a07
mhmdho <hadiomidm@gmail.com>
2022-05-17 17:09:57 +0430

    user admin.py: Set admin page fields for user

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 
 # Register your models here.
 
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    search_fields = ('phone', 'email')
+    list_filter = ('is_active', 'is_phone_verified')
+    list_display = ('email', 'phone', 'last_login', 'date_joined',
+                    'is_active', 'is_phone_verified')
+    date_hierarchy = ('last_login')
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'phone', ('password1', 'password2'))
+        }),
+    )
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'phone', 'password')
+        }),
+
+        ('more options', {
+            'classes': ('collapse',),
+            'fields':  ('first_name', 'last_name'),
+        }),
+    )
+
+    save_on_top =True
+
+    list_editable = ('is_active',)
+
+    ordering = ()
+
+admin.site.register(CustomUser, CustomUserAdmin)
