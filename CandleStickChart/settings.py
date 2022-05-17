eb44326e95f11cf9d5a28eaa36072790d83673da
mhmdho <hadiomidm@gmail.com>
2022-05-17 19:30:55 +0430

    settings.py: fix swagger bug

--- CandleStickChart/settings.py
+++ CandleStickChart/settings.py
@@ -43,8 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.user',
     'apps.trade',
-
-    'django.contrib.staticfiles',  # required for serving swagger ui's css/js files
     'drf_yasg',
 ]
 
