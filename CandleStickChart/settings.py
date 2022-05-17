eacc883b3eac8ae0bac0d69cbcf9c99696d9c7ce
mhmdho <hadiomidm@gmail.com>
2022-05-17 15:32:41 +0430

    Set apps settings in settings.py and apps.py of user and trade

--- CandleStickChart/settings.py
+++ CandleStickChart/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.user',
+    'apps.trade',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.CustomUser'
