cfc75e56bca3752804a21b1f8cb98c8c6ca60e76
mhmdho <hadiomidm@gmail.com>
2022-05-17 18:32:07 +0430

    Set user app url in project urls.py / Set UserRegister urls & view

---
+++ apps/user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import UserRegister
+
+
+urlpatterns = [
+    path('register/', UserRegister.as_view(), name='register_url'),
+
+]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+
+from rest_framework.response import Response
+from .models import CustomUser
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer
+from rest_framework import generics
+from rest_framework.parsers import FormParser, MultiPartParser
+
 
 # Create your views here.
+
+
+class UserRegister(generics.CreateAPIView):
+    """
+    Takes a set of informations and register customer.
+    """
+    queryset = CustomUser.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        self.create(request, *args, **kwargs)
+        return Response({"Success": "Your registration was successful"}, status=status.HTTP_201_CREATED)
