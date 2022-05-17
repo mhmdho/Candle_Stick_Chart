e0d9078eadb9ea803b4ff80c6e26371c76c59606
mhmdho <hadiomidm@gmail.com>
2022-05-17 17:48:51 +0430

    Set user register serializer

---
+++ apps/user/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from .models import CustomUser
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'phone', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create(
+            email=validated_data['email'],
+            phone=validated_data['phone'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
