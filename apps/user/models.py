95b08114f51048b3afe394faf149be5cc7a2987e
mhmdho <hadiomidm@gmail.com>
2022-05-17 15:38:22 +0430

    user models.py: Rename User to CustomUser, fix some minor bugs

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.core.validators import RegexValidator
 
 
 # Create your models here.
 
 
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     """
     Customize django user and add phone & is_phone_verified to it.
     """
