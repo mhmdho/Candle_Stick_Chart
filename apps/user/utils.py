1264f51bfda2820583102019d80f2b25dee1009d
mhmdho <hadiomidm@gmail.com>
2022-05-28 18:05:06 +0430

    utils.py: define get_qrcode for OTP class

--- apps/user/utils.py
+++ apps/user/utils.py
@@ -4,22 +4,23 @@ import onetimepass as otp
 import random
 import time
 import re
+import qrcode
 
 
 class OTP:
     """
     Generate otp based on user phone number.
     """
-    def __init__(self, phone):     
-        self.secret = phone + OTP_SECRET
+    def __init__(self, phone):
+        self.secret = re.sub(r'[^a-zA-Z2-7]', '',
+                        phone + OTP_SECRET)[:16]
         self.digit = 6
-        self.interval = 120
+        self.interval = 60
         self.verified = False
 
     @property
     def totp(self):
-        my_secret = re.sub(r'[^a-zA-Z2-7]', '', self.secret)[:16]
-        my_token = otp.get_totp(my_secret, token_length=self.digit,
+        my_token = otp.get_totp(self.secret, token_length=self.digit,
                                 interval_length=self.interval)
         self.expire = time.time() + self.interval
         rn_token = random.randint(10000,9999999) + my_token
@@ -28,3 +29,8 @@ class OTP:
     @property
     def expire_at(self):
         return datetime.fromtimestamp(self.expire).strftime('%H:%M:%S')
+
+    def get_qrcode(self, email):
+        img = qrcode.make(f'otpauth://totp/{email}?secret={self.secret}&\
+            algorithm=SHA1&digits={self.digit}&period={self.interval}&issuer=TradingHills')
+        return img.show()
